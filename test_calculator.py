1060ece8eb6a1e96f58ed44caf5258991505cd1b
SamruddhiNareshsakharkar <samruddhisakharkar04@gmail.com>
2026-06-03 11:30:47 +0530

    added calc again file   commit

--- test_calculator.py
+++ test_calculator.py
@@ -3,4 +3,15 @@ from calculator import add
 
 def test_add():   
     assert add(1,2) ==3
-    assert add
+    assert add(3,4) ==7
+
+def test_sub():
+    assert sub(5,2) ==3
+    assert sub(10,4) == 6
+    assert sub(7,3) == 4
+
+def test_mul():
+    assert mul(2,3) == 6
+    assert mul(4,5) == 24
+    assert mul (3,3) == 10
+    
